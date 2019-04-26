6579d7255e296aac9685d367dd3f9be5d16d74db
Pablo Felipe <pablo.soto29@gmail.com>
2019-04-26 03:16:29 -0400

    Update horario_flask.py

--- horario_flask.py
+++ horario_flask.py
@@ -8,34 +8,38 @@ import json
 
 app = Flask(__name__)
 
+@app.route("/")
+def bienvenida():
+    return "Ingresa tu rut en /rut/<rut>"
+
 @app.route("/rut/<rut>")
-def hola(rut):
+def get_horario(rut):
     source = requests.get('https://intranet.ufro.cl/horario/horario_alumno_detalle2.php?matricula='+rut+'&periodo=10&dia=1&ano=2019&nro_semest=1&estado=N').text
-    horario = []
-    hora = {
+    detalle = []
+    horario = {
         "dia": "",
         "periodo": "",
         "hora_inicio": "",
         "hora_termino": "",
         "tipo_clase": "",
         "fecha_inicio": "",
-        "fecha_termino": "d",
-        "sector": "g",
+        "fecha_termino": "",
+        "sector": "",
         "sala": "",
         "asignatura": "",
-        "docente": "1",
+        "docente": "",
     }
 
     soup = BeautifulSoup(source, 'lxml')
-    for detalle in soup.find_all('td'):
-        horario.append(detalle.text.strip())
+    for dato in soup.find_all('td'):
+        detalle.append(dato.text.strip())
 
     i = 0
-    for x in hora:
-        hora[x] = horario[i]
+    for x in horario:
+        horario[x] = detalle[i]
         i+=1
     
-    return json.dumps(hora, ensure_ascii=False)
+    return json.dumps(horario, ensure_ascii=False)
 
 
 if __name__ == '__main__':
