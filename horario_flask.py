4698daa3055a71b79b77ad0e8b2e1d9d2e95c850
Pablo Felipe <pablo.soto29@gmail.com>
2019-05-17 00:25:30 -0400

    codigo optimizado

--- horario_flask.py
+++ horario_flask.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def bienvenida():
     return "Bienvenido, ingresa a /dia/rut/periodo para ver un período específico."
 
-@app.route("/<dia>/<rut>")
+@app.route("/di")
 def horario_lunes(dia,rut):
     time_init = time.time()
     periodos = []
@@ -62,52 +62,59 @@ def horario_lunes(dia,rut):
 
     else: periodos.append({"Error 202": "Ingresa día válido (1 - 6, de lunes a sábado, respectivamente)"})
 
-    
-
     # Encoding periodos a json
     json_horario = json.dumps(periodos, ensure_ascii=False, indent=4)
 
     return Response(json_horario, mimetype='application/json')
 
 
-@app.route("/<dia>/<rut>/<periodo>")
-def get_horario(rut, dia, periodo):
-    url = 'https://intranet.ufro.cl/horario/horario_alumno_detalle2.php?matricula={}&periodo={}&dia={}&ano=2019&nro_semest=1&estado=N'.format(rut,periodo,dia)
-    source = requests.get(url).text
-    detalle = []
-    horario = {
-        "dia": "",
-        "periodo": "",
-        "hora_inicio": "",
-        "hora_termino": "",
-        "tipo_clase": "",
-        "fecha_inicio": "",
-        "fecha_termino": "",
-        "sector": "",
-        "sala": "",
-        "asignatura": "",
-        "docente": ""
-    }
 
-    soup = BeautifulSoup(source, 'lxml')
-    # Búsqueda de etiquetas HTML td
-    for dato in soup.find_all('td'):
-        detalle.append(dato.text.strip())
+@app.route("/<dia>/<matricula>")
+def get_horario(dia, matricula):
+    time_init = time.time()
+    horario = []
 
-    # Llenado de dict
-    if len(detalle) == 11:
-        i = 0
-        for x in horario:
-            horario[x] = detalle[i]
-            i+=1
+    if 0 < int(dia) < 7:
+        for i in range(1,12):
+            periodo = {
+                "dia": "",
+                "periodo": "",
+                "hora_inicio": "",
+                "hora_termino": "",
+                "tipo_clase": "",
+                "fecha_inicio": "",
+                "fecha_termino": "",
+                "sector": "",
+                "sala": "",
+                "asignatura": "",
+                "docente": ""
+            }
+            url = 'https://intranet.ufro.cl/horario/horario_alumno_detalle2.php?matricula={}&periodo={}&dia={}&ano=2019&nro_semest=1&estado=N'.format(matricula,i,dia)
+            bs = BeautifulSoup(requests.get(url).text, 'lxml')
+            
+            buscar = bs.find_all('td')
+            print(len(buscar))
+            if len(buscar) == 11:
+                for i,(k,v) in enumerate(periodo.items()):
+                        periodo[k] = buscar[i].text.strip()
+                horario.append(periodo)
+            
 
-        horario = json.dumps(horario)
-        horario_json = json.loads(horario)
-    # En caso de período vacío        
-    else: horario_json = {404 : "ERROR!"}
+        # Caso: día sin clases
+        if len(horario) == 0:
+            horario.append({200: "No tienes clases este día"})
+    else:
+        horario.append({202: "Ingresa día válido (1 - 6, de lunes a sábado, respectivamente)"})
     
-    return Response(json.dumps(horario_json, ensure_ascii=False, indent=4), mimetype='application/json')
+    # Calcula tiempo de ejecución
+    time_finish = time.time()
+    time_dif = time_finish-time_init
+    horario.append({"Ejecutado en": "{} segundos".format(round(time_dif,3))})
+
+    # Encode a json
+    json_horario = json.dumps(horario, ensure_ascii=False, indent=4)
     
+    return Response(json_horario, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
